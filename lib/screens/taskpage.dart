d042f13e40ac394406e888d46d250cc9c404d261
kisekifrans <agisswag23@gmail.com>
2022-06-26 00:30:09 +0800

    Update coyyy

--- lib/screens/taskpage.dart
+++ lib/screens/taskpage.dart
@@ -20,13 +20,34 @@ class _TaskpageState extends State<Taskpage> {
   int _taskId = 0;
   String _taskTitle = "";
 
+  late FocusNode _titleFocus;
+  late FocusNode _descriptionFocus;
+  late FocusNode _todoFocus;
+
+  bool _contentVisible = false;
+
   @override
   void initState() {
     if (widget.task != null) {
+      _contentVisible = true;
       _taskTitle = widget.task!.title!;
       _taskId = widget.task!.id!;
     }
+
+    _titleFocus = FocusNode();
+    _descriptionFocus = FocusNode();
+    _todoFocus = FocusNode();
+
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _titleFocus.dispose();
+    _descriptionFocus.dispose();
+    _todoFocus.dispose();
+
+    super.dispose();
   }
 
   @override
@@ -61,6 +82,7 @@ class _TaskpageState extends State<Taskpage> {
                         ),
                         Expanded(
                           child: TextField(
+                            focusNode: _titleFocus,
                             onSubmitted: (value) async {
                               if (value != "") {
                                 //Mengecek apakah task kosong.
@@ -71,10 +93,16 @@ class _TaskpageState extends State<Taskpage> {
                                     title: value,
                                   ); //membuat instance task
 
-                                  await _dbHelper.insertTask(_newTask);
+                                  _taskId =
+                                      await _dbHelper.insertTask(_newTask);
+                                  setState(() {
+                                    _contentVisible = true;
+                                    _taskTitle = value;
+                                  });
                                 } else {
                                   print("Update dulu ges tasknya..");
                                 }
+                                _descriptionFocus.requestFocus();
                               }
                             },
                             controller: TextEditingController()
@@ -93,118 +121,137 @@ class _TaskpageState extends State<Taskpage> {
                       ],
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      bottom: 12.0,
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
+                  Visibility(
+                    visible: _contentVisible,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 12.0,
+                      ),
+                      child: TextField(
+                        onSubmitted: (value) {
+                          _todoFocus.requestFocus();
+                        },
+                        focusNode: _descriptionFocus,
+                        decoration: InputDecoration(
                           hintText: "Masukan Deskripsi...",
                           border: InputBorder.none,
                           contentPadding: EdgeInsets.symmetric(
                             horizontal: 24.0,
-                          )),
-                    ),
-                  ),
-                  FutureBuilder(
-                    initialData: [],
-                    future: _dbHelper.getTodo(_taskId),
-                    builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-                      return Expanded(
-                        child: ListView.builder(
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (context, index) {
-                            return GestureDetector(
-                              onTap: () {
-                                //Biar tercentang..
-                              },
-                              child: TodoWidget(
-                                text: snapshot.data![index].title,
-                                isDone: snapshot.data![index].isDone == 0
-                                    ? false
-                                    : true,
-                              ),
-                            );
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24.0,
-                    ),
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 20.0,
-                          height: 20.0,
-                          margin: EdgeInsets.only(
-                            right: 10.0,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(5.0),
-                            border: Border.all(
-                              color: Color(0xFF86829D),
-                              width: 1.5,
-                            ),
-                          ),
-                          child: Image(
-                            image: AssetImage(
-                              'assets/images/check_icon.png',
-                            ),
                           ),
                         ),
-                        Expanded(
-                          child: TextField(
-                            onSubmitted: (value) async {
-                              if (value != "") {
-                                if (widget.task != null) {
-                                  DatabaseHelper _dbHelper = DatabaseHelper();
-                                  Todo _newTodo = Todo(
-                                    title: value,
-                                    isDone: 0,
-                                    taskId: widget.task!.id,
-                                  );
-                                  await _dbHelper.insertTodo(_newTodo);
-                                  setState(() {});
-                                } else {
-                                  print("muncul kalau gamau");
-                                }
-                              }
+                      ),
+                    ),
+                  ),
+                  Visibility(
+                    visible: _contentVisible,
+                    child: FutureBuilder(
+                      initialData: [],
+                      future: _dbHelper.getTodo(_taskId),
+                      builder:
+                          (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                        return Expanded(
+                          child: ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  //Biar tercentang..
+                                },
+                                child: TodoWidget(
+                                  text: snapshot.data![index].title,
+                                  isDone: snapshot.data![index].isDone == 0
+                                      ? false
+                                      : true,
+                                ),
+                              );
                             },
-                            decoration: InputDecoration(
-                              hintText: "Masukan Todo's...",
-                              border: InputBorder.none,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Visibility(
+                    visible: _contentVisible,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24.0,
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 20.0,
+                            height: 20.0,
+                            margin: EdgeInsets.only(
+                              right: 10.0,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(5.0),
+                              border: Border.all(
+                                color: Color(0xFF86829D),
+                                width: 1.5,
+                              ),
+                            ),
+                            child: Image(
+                              image: AssetImage(
+                                'assets/images/check_icon.png',
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                          Expanded(
+                            child: TextField(
+                              focusNode: _todoFocus,
+                              onSubmitted: (value) async {
+                                if (value != "") {
+                                  if (widget.task != null) {
+                                    DatabaseHelper _dbHelper = DatabaseHelper();
+                                    Todo _newTodo = Todo(
+                                      title: value,
+                                      isDone: 0,
+                                      taskId: widget.task!.id,
+                                    );
+                                    await _dbHelper.insertTodo(_newTodo);
+                                    setState(() {});
+                                  } else {
+                                    print("muncul kalau gamau");
+                                  }
+                                }
+                              },
+                              decoration: InputDecoration(
+                                hintText: "Masukan Todo's...",
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
-              Positioned(
-                bottom: 24.0,
-                right: 24.0,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Taskpage()),
-                    );
-                  },
-                  child: Container(
-                    width: 60.0,
-                    height: 60.0,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 242, 58, 58),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Image(
-                      image: AssetImage(
-                        'assets/images/delete_icon.png',
+              Visibility(
+                visible: _contentVisible,
+                child: Positioned(
+                  bottom: 24.0,
+                  right: 24.0,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Taskpage()),
+                      );
+                    },
+                    child: Container(
+                      width: 60.0,
+                      height: 60.0,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 242, 58, 58),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Image(
+                        image: AssetImage(
+                          'assets/images/delete_icon.png',
+                        ),
                       ),
                     ),
                   ),
